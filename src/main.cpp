02379c4e7766aec9d1ecc56829628107aadb1edf
Yobyeh <yobyeh@gmail.com>
2023-12-08 01:18:24 -0500

    bouncing working

--- src/main.cpp
+++ src/main.cpp
@@ -103,57 +103,9 @@ void loadNextfile();
 void printDirectory(File, int);
 void shootingStarAnimation(int, int, int, int, int, int);
 void runStars();
+void bouncingColoredBalls(int BallCount, byte colors[][3]);
 
 //void playProgram(int);
-
-//balls
-void BouncingColoredBalls(int BallCount, byte colors[][3]) {
-  float Gravity = -9.81;
-  int StartHeight = 1;
- 
-  float Height[BallCount];
-  float ImpactVelocityStart = sqrt( -2 * Gravity * StartHeight );
-  float ImpactVelocity[BallCount];
-  float TimeSinceLastBounce[BallCount];
-  int   Position[BallCount];
-  long  ClockTimeSinceLastBounce[BallCount];
-  float Dampening[BallCount];
- 
-  for (int i = 0 ; i < BallCount ; i++) {  
-    ClockTimeSinceLastBounce[i] = millis();
-    Height[i] = StartHeight;
-    Position[i] = 0;
-    ImpactVelocity[i] = ImpactVelocityStart;
-    TimeSinceLastBounce[i] = 0;
-    Dampening[i] = 0.90 - float(i)/pow(BallCount,2);
-  }
-
-  while (true) {
-    for (int i = 0 ; i < BallCount ; i++) {
-      TimeSinceLastBounce[i] =  millis() - ClockTimeSinceLastBounce[i];
-      Height[i] = 0.5 * Gravity * pow( TimeSinceLastBounce[i]/1000 , 2.0 ) + ImpactVelocity[i] * TimeSinceLastBounce[i]/1000;
- 
-      if ( Height[i] < 0 ) {                      
-        Height[i] = 0;
-        ImpactVelocity[i] = Dampening[i] * ImpactVelocity[i];
-        ClockTimeSinceLastBounce[i] = millis();
- 
-        if ( ImpactVelocity[i] < 0.01 ) {
-          ImpactVelocity[i] = ImpactVelocityStart;
-        }
-      }
-      Position[i] = round( Height[i] * (NUM_LEDS - 1) / StartHeight);
-    }
- 
-    for (int i = 0 ; i < BallCount ; i++) {
-      //leds[i] = CRGB(red, green, blue);
-      leds[Position[i]] = CRGB(colors[i][0],colors[i][1],colors[i][2]);
-    }
-   
-    FastLED.show();
-    ledClear();
-  }
-}
 
 void setup() {
   delay(5000); // sanity delay
@@ -230,7 +182,7 @@ void loop(){
       //runStars();
       byte colors[3][3] = { {0xff, 0,0},
                         {0xff, 0xff, 0xff},
-                        {0   , 0   , 0xff} };
+                        {0, 0xdd, 0} };
 
       bouncingColoredBalls(3, colors);
       
@@ -335,7 +287,7 @@ void ledClear(){
     leds[i] = 0x000000;
   }
   FastLED.show();
-  lcdWrite("Clear", "LEDs", 1500);
+  //lcdWrite("Clear", "LEDs", 1500);
 }
 
 void ledTest(){
@@ -493,7 +445,54 @@ void setBrightness(int brightness){
 }
 
 
+//balls
+void bouncingColoredBalls(int BallCount, byte colors[][3]) {
+  float Gravity = -9.81;
+  int StartHeight = 300;
+ 
+  float Height[BallCount];
+  float ImpactVelocityStart = sqrt( -2 * Gravity * StartHeight );
+  float ImpactVelocity[BallCount];
+  float TimeSinceLastBounce[BallCount];
+  int   Position[BallCount];
+  long  ClockTimeSinceLastBounce[BallCount];
+  float Dampening[BallCount];
+ 
+  for (int i = 0 ; i < BallCount ; i++) {  
+    ClockTimeSinceLastBounce[i] = millis();
+    Height[i] = StartHeight;
+    Position[i] = 0;
+    ImpactVelocity[i] = ImpactVelocityStart;
+    TimeSinceLastBounce[i] = 0;
+    Dampening[i] = 0.90 - float(i)/pow(BallCount,2);
+  }
 
+  while (true) {
+    for (int i = 0 ; i < BallCount ; i++) {
+      TimeSinceLastBounce[i] =  millis() - ClockTimeSinceLastBounce[i];
+      Height[i] = 0.5 * Gravity * pow( TimeSinceLastBounce[i]/1000 , 2.0 ) + ImpactVelocity[i] * TimeSinceLastBounce[i]/1000;
+ 
+      if ( Height[i] < 0 ) {                      
+        Height[i] = 0;
+        ImpactVelocity[i] = Dampening[i] * ImpactVelocity[i];
+        ClockTimeSinceLastBounce[i] = millis();
+ 
+        if ( ImpactVelocity[i] < 0.01 ) {
+          ImpactVelocity[i] = ImpactVelocityStart;
+        }
+      }
+      Position[i] = round( Height[i] * ((NUM_LEDS - 1)) / StartHeight);
+    }
+ 
+    for (int i = 0 ; i < BallCount ; i++) {
+      //leds[i] = CRGB(red, green, blue);
+      leds[Position[i]] = CRGB(colors[i][0],colors[i][1],colors[i][2]);
+    }
+   
+    FastLED.show();
+    ledClear();
+  }
+}
 
 //shooting star
 void runStars(){
